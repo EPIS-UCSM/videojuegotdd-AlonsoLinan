5a285cde1a6497f3b24d96a53c2bfd1162fde717
AlonsoLinan <gustavo.linan@ucsm.edu.pe>
2022-07-07 20:41:10 -0500

    Version Final

--- Frogger/actors.py
+++ Frogger/actors.py
@@ -12,6 +12,10 @@ import random
 import pygame
 from pygame.locals import *
 
+#Importamos Clases y Métodos del archivo frogger.py
+from frogger import *
+
+#Actores en el videojuego
 
 #Clase Rectángulo (forma de todos los objetos)
 class Rectangle:
@@ -34,3 +38,144 @@ class Rectangle:
 		obottom = other.y + other.h
 
 		return not (left >= oright or right <= oleft or top >= obottom or bottom <= otop)
+
+#Clase Lane (Carriles de fondo)
+class Lane(Rectangle):
+
+	def __init__(self, y, c=None, n=0, l=0, spc=0, spd=0):
+		super(Lane, self).__init__(0, y * g_vars['grid'], g_vars['width'], g_vars['grid'])
+		self.type = t
+		self.color = c
+		self.obstacles = []
+		offset = random.uniform(0, 200)
+		if self.type == 'car':
+			o_color = (128, 128, 128)
+		if self.type == 'log':
+			o_color = (185, 122, 87)
+		for i in range(n):
+			self.obstacles.append(Obstacle(offset + spc * i, 
+				y * g_vars['grid'], l * g_vars['grid'], g_vars['grid'], spd, o_color))
+
+#Clase Frog (sapo que controla el jugador)
+class Frog(Rectangle):
+
+	def __init__(self, x, y, w):
+		super(Frog, self).__init__(x, y, w, w)
+		self.x0 = x
+		self.y0 = y
+		self.color = (34, 177, 76)
+		self.attached = None
+
+	def reset(self):
+		self.x = self.x0
+		self.y = self.y0
+		self.attach(None)
+
+	def move(self, xdir, ydir):
+		self.x += xdir * g_vars['grid']
+		self.y += ydir * g_vars['grid']
+
+	def attach(self, obstacle):	
+		self.attached = obstacle
+
+	def update(self):
+		if self.attached is not None:
+			self.x += self.attached.speed
+
+		if self.x + self.w > g_vars['width']:
+			self.x = g_vars['width'] - self.w
+		
+		if self.x < 0:
+			self.x = 0
+		if self.y + self.h > g_vars['width']:
+			self.y = g_vars['width'] - self.w
+		if self.y < 0:
+			self.y = 0
+
+	def draw(self):
+		rect = Rect( [self.x, self.y], [self.w, self.h] )
+		pygame.draw.rect( g_vars['window'], self.color, rect )
+
+#Clase Obstacle (Obstáculos que esquiva el jugador para ganar o que colisiona para perder)
+class Obstacle(Rectangle):
+
+	def __init__(self, x, y, w, h, s, c):
+		super(Obstacle, self).__init__(x, y, w, h)
+		self.color = c
+		self.speed = s
+
+	def update(self):
+		self.x += self.speed
+		if self.speed > 0 and self.x > g_vars['width'] + g_vars['grid']:
+			self.x = -self.w
+		elif self.speed < 0 and self.x < -self.w:
+			self.x = g_vars['width']
+
+	def draw(self):
+		pygame.draw.rect( g_vars['window'], self.color, Rect( [self.x, self.y], [self.w, self.h] ) )
+
+
+#Clase Lane (Carriles con los objetos Obstáculo)
+class Lane(Rectangle):
+
+	def __init__(self, y, t='safety', c=None, n=0, l=0, spc=0, spd=0):
+		super(Lane, self).__init__(0, y * g_vars['grid'], g_vars['width'], g_vars['grid'])
+		self.type = t
+		self.color = c
+		self.obstacles = []
+		offset = 0#random.uniform(0, 200)
+		if self.type == 'car':
+			o_color = (128, 128, 128)
+		if self.type == 'log':
+			o_color = (185, 122, 87)
+		for i in range(n):
+			self.obstacles.append(Obstacle(offset + spc * i, 
+				y * g_vars['grid'], l * g_vars['grid'], g_vars['grid'], spd, o_color))
+
+	def check(self, frog):
+		checked = False
+		attached = False
+		frog.attach(None)
+		for obstacle in self.obstacles:
+			if frog.intersects(obstacle):
+				if self.type == 'car':
+					frog.reset()
+					checked = True
+				if self.type == 'log':
+					attached = True
+					frog.attach(obstacle)
+		if not attached and self.type == 'log':
+			frog.reset()
+			checked = True
+		return checked
+
+	def update(self):
+		for obstacle in self.obstacles:
+			obstacle.update()
+
+	def draw(self):
+		if self.color is not None:
+			pygame.draw.rect( g_vars['window'], self.color, Rect( [self.x, self.y], [self.w, self.h] ) )
+		for obstacle in self.obstacles:
+			obstacle.draw()
+
+
+#Clase Score (Puntaje Actual y Puntaje Máximo del jugador)
+class Score:
+
+	def __init__(self):
+		self.score = 0
+		self.high_score = 0
+		self.high_lane = 1
+		self.lives = 3
+
+	def update(self, points):
+		self.score += points
+		if self.score > self.high_score:
+			self.high_score = self.score
+
+	def reset(self):
+		self.score = 0
+		self.high_lane = 1
+		self.lives = 3
+		
--- Frogger/frogger.py
+++ Frogger/frogger.py
@@ -12,6 +12,9 @@ import random
 import pygame
 from pygame.locals import *
 
+#Importamos Clases y Métodos del archivo actors.py
+from actors import *
+
 #Declaramos las dimensiones de la pantalla del videojuego
 g_vars = {}
 g_vars['width'] = 416
@@ -19,3 +22,140 @@ g_vars['height'] = 416
 g_vars['fps'] = 30
 g_vars['grid'] = 32
 g_vars['window'] = pygame.display.set_mode( [g_vars['width'], g_vars['height']], pygame.HWSURFACE)
+
+
+#Clase App
+class App:
+
+	#Métodos de incicialización
+	def __init__(self):
+		pygame.init()
+		pygame.display.set_caption("Frogger")
+		
+		self.running = None
+		self.state = None
+		self.frog = None
+		self.score = None
+		self.lanes = None
+
+		self.clock = pygame.time.Clock()
+		self.font = pygame.font.SysFont('Courier New', 16)
+
+	def init(self):
+		self.running = True
+		self.state = 'START'
+		
+		self.frog = Frog(g_vars['width']/2 - g_vars['grid']/2, 12 * g_vars['grid'], g_vars['grid'])
+		self.frog.attach(None)
+		self.score = Score()
+
+		self.lanes = []
+		self.lanes.append( Lane( 1, c=( 50, 192, 122) ) )
+		self.lanes.append( Lane( 2, t='log', c=(153, 217, 234), n=2, l=6, spc=350, spd=1.2) )
+		self.lanes.append( Lane( 3, t='log', c=(153, 217, 234), n=3, l=2, spc=180, spd=-1.6) )
+		self.lanes.append( Lane( 4, t='log', c=(153, 217, 234), n=4, l=2, spc=140, spd=1.6) )
+		self.lanes.append( Lane( 5, t='log', c=(153, 217, 234), n=2, l=3, spc=230, spd=-2) )
+		self.lanes.append( Lane( 6, c=(50, 192, 122) ) )
+		self.lanes.append( Lane( 7, c=(50, 192, 122) ) )
+		self.lanes.append( Lane( 8, t='car', c=(195, 195, 195), n=3, l=2, spc=180, spd=-2) )
+		self.lanes.append( Lane( 9, t='car', c=(195, 195, 195), n=2, l=4, spc=240, spd=-1) )
+		self.lanes.append( Lane( 10, t='car', c=(195, 195, 195), n=4, l=2, spc=130, spd=2.5) )
+		self.lanes.append( Lane( 11, t='car', c=(195, 195, 195), n=3, l=3, spc=200, spd=1) )
+		self.lanes.append( Lane( 12, c=(50, 192, 122) ) )
+
+	#Métodos de acción
+	def event(self, event):
+		if event.type == QUIT:
+			self.running = False
+
+		if event.type == KEYDOWN and event.key == K_ESCAPE:
+			self.running = False
+
+		if self.state == 'START':
+			if event.type == KEYDOWN and event.key == K_RETURN:
+				self.state = 'PLAYING'
+
+		if self.state == 'PLAYING':
+			if event.type == KEYDOWN and event.key == K_LEFT:
+				self.frog.move(-1, 0)
+			if event.type == KEYDOWN and event.key == K_RIGHT:
+				self.frog.move(1, 0)
+			if event.type == KEYDOWN and event.key == K_UP:
+				self.frog.move(0, -1)
+			if event.type == KEYDOWN and event.key == K_DOWN:
+				self.frog.move(0, 1)
+
+	def update(self):
+		for lane in self.lanes:
+			lane.update()
+		
+		lane_index = self.frog.y // g_vars['grid'] - 1
+		if self.lanes[lane_index].check(self.frog):
+			self.score.lives -= 1
+			self.score.score = 0
+		
+		self.frog.update()
+
+		if (g_vars['height']-self.frog.y)//g_vars['grid'] > self.score.high_lane:
+			if self.score.high_lane == 11:
+				self.frog.reset()
+				self.score.update(200)
+			else:
+				self.score.update(10)
+				self.score.high_lane = (g_vars['height']-self.frog.y)//g_vars['grid']
+
+		if self.score.lives == 0:
+			self.frog.reset()
+			self.score.reset()
+			self.state = 'START'
+
+	#Métodos de diseño de interfaz
+	def draw(self):
+		g_vars['window'].fill( (0, 0, 0) )
+		if self.state == 'START':
+
+			self.draw_text("Frogger!", g_vars['width']/2, g_vars['height']/2 - 15, 'center')
+			self.draw_text("Presione ENTER para empezar a jugar.", 
+				g_vars['width']/2, g_vars['height']/2 + 15, 'center')
+
+		if self.state == 'PLAYING':
+
+			self.draw_text("Vidas: {0}".format(self.score.lives), 5, 8, 'left')
+			self.draw_text("Score: {0}".format(self.score.score), 120, 8, 'left')
+			self.draw_text("High Score: {0}".format(self.score.high_score), 240, 8, 'left')
+
+			for lane in self.lanes:
+				lane.draw()
+			self.frog.draw()
+
+		pygame.display.flip()
+
+	def draw_text(self, t, x, y, a):
+		text = self.font.render(t, False, (255, 255, 255))
+		if a == 'center':
+			x -= text.get_rect().width / 2
+		elif a == 'right':
+			x += text.get_rect().width
+		g_vars['window'].blit( text , [x, y])
+
+	#Método de reinicio
+	def cleanup(self):
+		pygame.quit()
+		quit()
+
+	#Método de ejecución
+	def execute(self):
+		if self.init() == False:
+			self.running = False
+		while self.running:
+			for event in pygame.event.get():
+				self.event( event )
+			self.update()
+			self.draw()
+			self.clock.tick(g_vars['fps'])
+		self.cleanup()
+
+#Ejecución del videojuego
+if __name__ == "__main__":
+	gameApp = App()
+	gameApp.execute()
